e68b459674ce763724af5b31dfda719b8a6dc88d
Yusuv <kingyusuwkillerspy@mail.ru>
2023-05-21 18:36:09 +0300

    Новая ветка 2

---
+++ Exemple001_HelloConsole/Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет," + username + ", сколько вам лет?");
+string let = Console.ReadLine();
+Console.WriteLine("Теперь мы знаем, что вам " + let);
