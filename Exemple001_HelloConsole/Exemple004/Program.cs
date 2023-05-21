b4e220f15061cae5c7526d3f83978dbf5d722cd5
Yusuv <kingyusuwkillerspy@mail.ru>
2023-05-21 19:00:19 +0300

    Улучшенный калькулятор

---
+++ Exemple001_HelloConsole/Exemple004/Program.cs
@@ -0,0 +1,5 @@
+﻿int number1 = new Random().Next(1,10);
+Console.WriteLine("Первое число: " + number1);
+int number2 = new Random().Next(1,10);
+Console.WriteLine("Второе число: " + number2);
+Console.WriteLine(number1 / number2);
